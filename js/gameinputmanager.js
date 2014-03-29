011a82d96c83f0b4a4e01b4e158fa766d2c7893b
Geoffrey Sangston <geoffreysangston@gmail.com>
2014-03-29 13:21:28 -0400

    testing iphone

--- js/gameinputmanager.js
+++ js/gameinputmanager.js
@@ -61,6 +61,7 @@ GameInputManager.prototype.listen = function() {
 		gameContainer.addEventListener(this.eventTouchmove, function(e){
 			e.preventDefault();
 			if(dragging){
+				console.log(e.x);
 				var mouseX = e.x - gameContainer.offsetLeft;
 				var mouseY = e.y - gameContainer.offsetTop;
 				var data = {
